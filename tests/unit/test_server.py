4046597e69fcd4391271470228d89f284d561577
ThiHieuLUU <thihieuluu@gmail.com>
2021-06-25 17:31:54 +0200

    TDD tests for checking validated email

---
+++ tests/unit/test_server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/venv python3
+
+import pytest
+from server import app, load_clubs, load_competitions
+
+clubs = load_clubs()
+competitions = load_competitions()
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+
+    yield app.test_client()  # tests run here
+
+def login(client, email):
+    return client.post("/showSummary", data=dict(
+        email=email,
+    ), follow_redirects=True)
+
+
+def test_login_for_known_email__success(client):
+    club = clubs[0]
+    # Test case for a known email
+    email = club["email"]
+
+    response = login(client, email)
+    assert response.status_code == 200
+    assert b"<title>Summary | GUDLFT Registration</title>" in response.data
+    assert b"Logout" in response.data
+    string = f'<h2>Welcome, {email} </h2>'
+    assert str.encode(string) in response.data
+
+
