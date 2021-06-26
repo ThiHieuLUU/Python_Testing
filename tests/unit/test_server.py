f6cbc89bac8acc49ca3b75f2cbdc454923b9a14e
ThiHieuLUU <thihieuluu@gmail.com>
2021-06-27 01:42:49 +0200

    Delete fixture for clubs and competitions

--- tests/unit/test_server.py
+++ tests/unit/test_server.py
@@ -10,6 +10,9 @@ from server import (
     MAX_PLACES
 )
 
+clubs = load_clubs()
+competitions = load_competitions()
+
 
 @pytest.fixture
 def client():
@@ -18,25 +21,13 @@ def client():
 
 
 @pytest.fixture
-def clubs():
-    """For the tests using updated clubs."""
-    return load_clubs()
-
-
-@pytest.fixture
-def competitions():
-    """For the tests using updated competitions."""
-    return load_competitions()
-
-
-@pytest.fixture
-def club(clubs):
+def club():
     """For the tests using the first club."""
     return clubs[0]
 
 
 @pytest.fixture
-def competition(competitions):
+def competition():
     """For the tests using the first competition."""
     return competitions[0]
 
@@ -104,7 +95,7 @@ def test_purchase_places__success(client, club, competition):
     assert b"Great - booking complete!" in response.data
 
 
-def test_purchase_places_using_more_than_club_points__failure(client, clubs, competitions):
+def test_purchase_places_using_more_than_club_points__failure(client, club, competition):
     """
     GIVEN a club logged in
     WHEN the secretary wants to book places which is greater than the club's points
@@ -114,17 +105,17 @@ def test_purchase_places_using_more_than_club_points__failure(client, clubs, com
     # Make sure that club's points is smaller than MAX_PLACES and competition's available places
     # in order to receive the corresponding error message
 
-    clubs[0]["points"] = MAX_PLACES - 1
-    competitions[0]["number_of_places"] = MAX_PLACES
+    club["points"] = MAX_PLACES - 1
+    competition["number_of_places"] = MAX_PLACES
 
     update_clubs_json({"clubs": clubs})
     update_competitions_json({"competitions": competitions})
 
-    clubs = load_clubs()
-    competitions = load_competitions()
+    clubs_updated = load_clubs()
+    competitions_updated = load_competitions()
 
-    club = clubs[0]
-    competition = competitions[0]
+    club = clubs_updated[0]
+    competition = competitions_updated[0]
 
     club_name = club["name"]
     competition_name = competition['name']
@@ -141,7 +132,7 @@ def test_purchase_places_using_more_than_club_points__failure(client, clubs, com
     assert b"You can't book more than your available points!" in response.data
 
 
-def test_purchase_more_than_12_places_per_competition__failure(client, clubs, competitions):
+def test_purchase_more_than_12_places_per_competition__failure(client, club, competition):
     """
     GIVEN a club logged in
     WHEN the secretary wants to book more than 12 places per competition
@@ -150,17 +141,17 @@ def test_purchase_more_than_12_places_per_competition__failure(client, clubs, co
     # Make sure that MAX_PLACES is smaller than club's points and competition's available places
     # in order to receive the corresponding error message
 
-    clubs[0]["points"] = MAX_PLACES + 1
-    competitions[0]["number_of_places"] = MAX_PLACES + 2
+    club["points"] = MAX_PLACES + 1
+    competition["number_of_places"] = MAX_PLACES + 2
 
     update_clubs_json({"clubs": clubs})
     update_competitions_json({"competitions": competitions})
 
-    clubs = load_clubs()
-    competitions = load_competitions()
+    clubs_updated = load_clubs()
+    competitions_updated = load_competitions()
 
-    club = clubs[0]
-    competition = competitions[0]
+    club = clubs_updated[0]
+    competition = competitions_updated[0]
 
     club_name = club["name"]
     competition_name = competition['name']
@@ -177,7 +168,7 @@ def test_purchase_more_than_12_places_per_competition__failure(client, clubs, co
     assert b"You can't book more than 12 places!" in response.data
 
 
-def test_purchase_more_than_available_places_of_competition__failure(client, clubs, competitions):
+def test_purchase_more_than_available_places_of_competition__failure(client, club, competition):
     """
     GIVEN a club logged in
     WHEN the secretary wants to book more than available places of a competition
@@ -186,17 +177,17 @@ def test_purchase_more_than_available_places_of_competition__failure(client, clu
     # Make sure that competition's available places is smaller than club's points and MAX_PLACES
     # in order to receive the corresponding error message
 
-    clubs[0]["points"] = MAX_PLACES
-    competitions[0]["number_of_places"] = MAX_PLACES - 1
+    club["points"] = MAX_PLACES
+    competition["number_of_places"] = MAX_PLACES - 1
 
     update_clubs_json({"clubs": clubs})
     update_competitions_json({"competitions": competitions})
 
-    clubs = load_clubs()
-    competitions = load_competitions()
+    clubs_updated = load_clubs()
+    competitions_updated = load_competitions()
 
-    club = clubs[0]
-    competition = competitions[0]
+    club = clubs_updated[0]
+    competition = competitions_updated[0]
 
     club_name = club["name"]
     competition_name = competition['name']
